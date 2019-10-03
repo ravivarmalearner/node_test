63457f121398a277886898a3f43a4e8ad7ba03b2
shaikh-raj <50153004+shaikh-raj@users.noreply.github.com>
2019-10-03 21:23:51 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+public class demo {
+
+	@Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+		assertEquals(0, 0);
+    }
+
+	@Test
+    public void multiplicationOfZeroIntegersShouldReturnZero2() {
+        assertEquals(2, 0);
+    }
+}
